a7bdc30e56621e87b94864c3d907c7e914e657bd
SeJongDeveloper <sejongdeveloper@gmail.com>
2020-12-27 17:33:43 +0900

    자바의정석 1회 배열 array 연습문제

---
+++ javaStudy/src/javaStudy/ch5/ex/Exercise5_1.java
@@ -0,0 +1,13 @@
+package javaStudy.ch5.ex;
+
+public class Exercise5_1 {
+	public static void main(String[] args) {
+		// int[] arr[];
+		// int[] arr = {1,2,3,};
+		// int[] arr = new int[5];
+		// int[] arr = new int[5]{1,2,3,4,5}; // new 연산자 대괄호에 배열크기 입력X (초기화 동시에 할 경우)
+		// int arr[5]; // 배열크기를 선언부에 했기 때문에 에러
+		// int[] arr[] = new int[3][];
+
+	}
+}
---
+++ javaStudy/src/javaStudy/ch5/ex/Exercise5_2.java
@@ -0,0 +1,7 @@
+package javaStudy.ch5.ex;
+
+public class Exercise5_2 {
+	public static void main(String[] args) {
+		System.out.println("2");
+	}
+}
