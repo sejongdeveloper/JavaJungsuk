542969c65b9e2273d1127fc102160842dccc40d2
SeJongDeveloper <sejongdeveloper@gmail.com>
2021-01-04 23:24:05 +0900

    자바의정석 1회 객체지향 프로그래밍2

---
+++ javaStudy/src/javaStudy/ch7/InstanceofTest.java
@@ -0,0 +1,24 @@
+package javaStudy.ch7;
+
+public class InstanceofTest {
+	public static void main(String[] args) {
+		FireEngine2 fe = new FireEngine2();
+		
+		if(fe instanceof FireEngine2) {
+			System.out.println("This is a FireEngine instance.");
+		}
+		
+		if(fe instanceof Car2) {
+			System.out.println("This is a Car instance.");
+		}
+		
+		if(fe instanceof Object) {
+			System.out.println("This is an Object instance.");
+		}
+		
+		System.out.println(fe.getClass().getName()); // 클래스의 이름을 출력
+	}
+} // class
+
+class Car2 {}
+class FireEngine2 extends Car2 {}
